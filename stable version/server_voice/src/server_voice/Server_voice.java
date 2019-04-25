6ef61d8bdb64d7154a3e9f3734a779536ea0c518
Dipta Das <dip9623@yahoo.com>
2019-04-25 20:38:28 +0600

    project push github

---
+++ stable version/server_voice/src/server_voice/Server_voice.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server_voice;
+
+/**
+ *
+ * @author dipta10
+ */
+public class Server_voice {
+
+    public static boolean calling = false;
+    
+    public static void main(String[] args) {
+        server_fr fr = new server_fr();
+        fr.setVisible(true);
+        fr.setResizable(false);
+    }
+    
+}
